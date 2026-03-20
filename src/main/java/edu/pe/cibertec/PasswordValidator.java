efc9dbb96d955954b048ffb6629fb9c45cb445a3
IJuanDiego <i202336416@cibertec.edu.pe>
2026-03-20 13:18:12 -0500

    Password validator PCI added

---
+++ src/main/java/edu/pe/cibertec/PasswordValidator.java
@@ -0,0 +1,17 @@
+package edu.pe.cibertec;
+
+public class PasswordValidator {
+
+    private static final int MIN_LENGHT = 7;
+    private static final String SPECIAL_CHARS = "!@#$%^&*()_+-=[]{}|;':,./<>?";
+
+    public boolean isValid(String password){
+        if(password == null || password.length()<MIN_LENGHT) return false;
+        if(password.contains(" ")) return false;
+        if(!hashUpperCase(password)) return false;
+        return  true;
+    }
+
+    private boolean hashUpperCase(String password)
+    { return password.chars().anyMatch(Character::isUpperCase);}
+}
